accdc6f127b78b9d6ad5994a315236cbe26d7cc6
BobLeClou <121858449+BobLeClou@users.noreply.github.com>
2023-05-23 17:09:57 +0200

    Add new page
    Ajout des nouvelles pages, en accord avec le PDF de Théo

---
+++ page03.php
@@ -0,0 +1,9 @@
+<!--Page Menu-->
+<?php include ('fonction.php');
+head();
+page_top();
+$nom_page = basename(__FILE__);
+navbar($nom_page);?>
+
+
+<?php page_bot();?>
---
+++ page05.php
@@ -0,0 +1,9 @@
+<!--Page Réservation-->
+<?php include ('fonction.php');
+head();
+page_top();
+$nom_page = basename(__FILE__);
+navbar($nom_page);?>
+
+
+<?php page_bot();?>
