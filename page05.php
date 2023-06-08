9b69fd1ab9210b14c0b6afb5b22a0696cfabd112
BaldFox1567 <chevalierprotheo@gmail.com>
2023-06-08 23:38:49 +0200

    modified:   page05.php

--- page05.php
+++ page05.php
@@ -1,9 +1,38 @@
 <!--Page Réservation-->
-<?php include 'fonction.php';
+<?php
+include 'fonction.php';
 head();
 page_top();
 $nom_page = basename(__FILE__);
-navbar($nom_page);?>
+navbar($nom_page);
+?>
 
+<div style="margin-left:50px;margin-right:50px;margin-top:30px;margin-bottom:20px; text-align:center">
+    <h2>Réservation</h2>
+    <p>Remplissez le formulaire ci-dessous pour réserver une table.</p>
+    <form action="process_reservation.php" method="POST">
+        <div class="form-group">
+            <label for="nom">Nom</label>
+            <input type="text" class="form-control" name="nom" id="nom" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" name="email" id="email" required>
+        </div>
+        <div class="form-group">
+            <label for="date">Date</label>
+            <input type="date" class="form-control" name="date" id="date" required>
+        </div>
+        <div class="form-group">
+            <label for="heure">Heure</label>
+            <input type="time" class="form-control" name="heure" id="heure" required>
+        </div>
+        <div class="form-group">
+            <label for="nb_personnes">Nombre de personnes</label>
+            <input type="number" class="form-control" name="nb_personnes" id="nb_personnes" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Réserver</button>
+    </form>
+</div>
 
 <?php page_bot();?>
